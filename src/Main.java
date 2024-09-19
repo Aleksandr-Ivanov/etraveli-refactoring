4054c7fefee64963b34a2cdc9d29785ed60e6ade
A. Ivanov <aleksandr.pv.ivanov@gmail.com>
2024-09-19 19:32:28 +0200

    Main class refactoring: Created backbone structure for ease of acceptance tests writing

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,74 @@
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
 
-  public static void main(String[] args) {
-    String expected = "Rental Record for C. U. Stomer\n\tYou've Got Mail\t3.5\n\tMatrix\t2.0\nAmount owed is 5.5\nYou earned 2 frequent points\n";
+  static class SlipItem {
+    String film;
+    double price;
 
-    String result = new RentalInfo().statement(new Customer("C. U. Stomer", Arrays.asList(new MovieRental("F001", 3), new MovieRental("F002", 1))));
-
-    if (!result.equals(expected)) {
-      throw new AssertionError("Expected: " + System.lineSeparator() + String.format(expected) + System.lineSeparator() + System.lineSeparator() + "Got: " + System.lineSeparator() + result);
+    public SlipItem(String film, double price) {
+      this.film = film;
+      this.price = price;
     }
+  }
+
+  public static void main(String[] args) {
+    String customerName = "C. U. Stomer";
+    List<MovieRental> rentals = Arrays.asList(
+        new MovieRental("F001", 3),
+        new MovieRental("F002", 1)
+    );
+    List<SlipItem> slipItems = Arrays.asList(
+        new SlipItem("You've Got Mail", 3.5),
+        new SlipItem("Matrix", 2.0)
+    );
+    double totalAmount = 5.5;
+    int frequentPoints = 2;
+
+    acceptanceTest(customerName, rentals, slipItems, totalAmount, frequentPoints);
 
     System.out.println("Success");
+  }
+
+  private static void acceptanceTest(String customerName,
+                                     List<MovieRental> rentals,
+                                     List<SlipItem> slipItems,
+                                     double totalAmount,
+                                     int frequentPoints) {
+
+    String resultSlip = new RentalInfo().statement(new Customer(customerName, rentals));
+
+    String expectedSlip = formExpectedSlip(customerName, slipItems, totalAmount, frequentPoints);
+
+    if (!resultSlip.equals(expectedSlip)) {
+      throw new AssertionError(String.format(
+          "%nExpected: %n%s%n%n" +
+          "Got: %n%s",
+          expectedSlip,
+          resultSlip
+      ));
+    }
+  }
+
+  private static String formExpectedSlip(String customerName,
+                                         List<SlipItem> slipItems,
+                                         double totalAmount,
+                                         int frequentPoints) {
+    String joinedSlipItems = slipItems.stream()
+        .map(item -> String.format("\t%s\t%.2f%n", item.film, item.price))
+        .collect(Collectors.joining());
+
+    return String.format(
+        "Rental Record for %s%n" +
+            "%s" +
+            "Amount owed is %.2f%n" +
+            "You earned %d frequent points%n",
+        customerName,
+        joinedSlipItems,
+        totalAmount,
+        frequentPoints
+    );
   }
 }
